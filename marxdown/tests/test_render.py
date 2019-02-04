def0718eec36649b62f40f7016ea964ec7991a64
erickpeirson <brp53@cornell.edu>
2019-02-04 11:09:36 -0500

    added render test

--- marxdown/tests/test_render.py
+++ marxdown/tests/test_render.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-
+import re
 from .. import render
 
 
@@ -20,3 +20,19 @@ class TestEscapeBrackets(TestCase):
                   **eprint        = "hep-ph/9609357"**
              }
             ```"""
+        expected = """
+            <div class="codehilite"><pre><span></span>        like this,
+            ```
+             @Article{{ '{' }}Beneke:1997hv,
+                  author    = &quot;M. Beneke and G. Buchalla and I. Dunietz&quot;,
+                  title     = &quot;{{ '{' }}Mixing induced CP asymmetries in inclusive B decays{{ '}' }}&quot;,
+                  journal   = &quot;Phys. Lett.&quot;,
+                  volume    = &quot;B393&quot;,
+                  year      = &quot;1997&quot;,
+                  pages     = &quot;132-142&quot;,
+                  **eprint        = &quot;hep-ph/9609357&quot;**
+             {{ '}' }}
+            ```
+            </pre></div>"""
+        self.assertEqual(re.sub("\s+", "", render.render(raw)),
+                         re.sub("\s+", "", expected))
