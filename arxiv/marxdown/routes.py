e99911237cc5bdd6f839b3f8bb7049f0ee053b5f
erickpeirson <brp53@cornell.edu>
2019-02-11 14:21:15 -0500

    cleaned up some typing

--- arxiv/marxdown/routes.py
+++ arxiv/marxdown/routes.py
@@ -1,4 +1,6 @@
-from typing import Dict, Callable
+"""URL routes for the marXdown application."""
+
+from typing import Dict, Callable, Optional, Tuple
 from urllib.parse import urljoin, urlparse, parse_qs, urlencode, urlunparse
 from werkzeug.urls import Href, url_encode, url_parse, url_unparse, url_encode
 
@@ -12,13 +14,18 @@ from arxiv import status
 from . import render
 from .services import site, index
 
+Response = Tuple[str, int, dict]
 
-def from_sitemap(page_path: str = ''):
+
+def from_sitemap(page_path: str = '') -> Response:
+    """Handle a request for ``page_path``."""
     try:
         page = site.load_page(page_path)
     except site.PageNotFound:
         raise NotFound('No such page')
 
+    # If static files are up in S3, we want to generate static URLs for S3
+    # rather than local ones.
     if current_app.config['FLASKS3_ACTIVE']:
         this_url_for = s3_url_for
     else:
@@ -33,12 +40,14 @@ def from_sitemap(page_path: str = ''):
         code = page.metadata['response'].get('status', status.HTTP_200_OK)
         if 'location' in page.metadata['response']:
             linker = render.get_linker(page, site.get_site_name())
-            route, kwarg, name, anchor = linker(page.metadata['response']['location'])
+            location_rel = page.metadata['response']['location']
+            route, kwarg, name, anchor = linker(location_rel)
             if kwarg is None:
                 location = route
             else:
                 if anchor is not None:
-                    location = this_url_for(route, **{kwarg: name}, _anchor=anchor)
+                    location = this_url_for(route, **{kwarg: name},
+                                            _anchor=anchor)
                 else:
                     location = this_url_for(route, **{kwarg: name})
             headers['Location'] = location
@@ -46,6 +55,8 @@ def from_sitemap(page_path: str = ''):
         if deleted:
             code = status.HTTP_404_NOT_FOUND
 
+    # Page metadata (populated from the frontmatter in the markdown source) is
+    # injected into the rendering context.
     context = dict(page.metadata)
     context.update({
         'page_path': page_path,
@@ -58,22 +69,21 @@ def from_sitemap(page_path: str = ''):
     return content, code, headers
 
 
-def search():
+def search() -> Response:
     """Handle a search request."""
     q = request.args.get('q')
     limit = min(int(request.args.get('l', 20)), 50)
-    page_number = int(request.args.get('p', 1))
-    if q:
-        results = index.find(q, page_number=page_number, limit=limit)
-    else:
-        results = None
+    page_no = int(request.args.get('p', 1))
+    results = index.find(q, page_number=page_no, limit=limit) if q else None
+
     site_name = site.get_site_name()
     title = f"Search {site.get_site_human_short_name()}"
     context = dict(results=results, q=q, site_name=site_name, pagetitle=title)
     try:
-        return render_template(f'{site_name}/search.html', **context)
+        data = render_template(f'{site_name}/search.html', **context)
     except jinja2.exceptions.TemplateNotFound:
-        return render_template('docs/search.html', **context)
+        data = render_template('docs/search.html', **context)
+    return data, status.HTTP_200_OK, {}
 
 
 def url_for_page_builder() -> Dict[str, Callable]:
@@ -91,6 +101,7 @@ def url_for_page_builder() -> Dict[str, Callable]:
 
 
 def get_blueprint(site_path: str, with_search: bool = True) -> Blueprint:
+    """Generate a blueprint for this site on the fly."""
     blueprint = Blueprint(site.get_site_name(), __name__,
                           url_prefix=site.get_url_prefix(),
                           static_folder=site.get_static_path(),
