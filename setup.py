96eb31143f455ad90e028fcd74b3dee02f1f933b
erickpeirson <brp53@cornell.edu>
2019-05-24 16:44:13 -0400

    incremented version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='arxiv-marxdown',
-    version='0.1.2',
+    version='0.1.5',
     packages=[f'arxiv.{package}' for package
               in find_packages('arxiv')],
     zip_safe=False,
