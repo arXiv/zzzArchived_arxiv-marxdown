09389cd9ea59c76c9dbe38b1f8a3295f9042cdbf
Martin Lessmeister <mhl10@cornell.edu>
2020-03-02 14:35:41 -0500

    Dependency updates (#31)
    * update arxiv-base to 0.16.6
    * update arxiv-base version in dockerfile
    * update arxiv-auth
    * pin gdb2

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='arxiv-marxdown',
-    version='0.2.4rc1',
+    version='0.2.4rc3',
     packages=[f'arxiv.{package}' for package
               in find_packages('arxiv')],
     zip_safe=False,
     install_requires=[
-        'arxiv-base==0.15.2',
-        'arxiv-auth==0.2.3',
+        'arxiv-base==0.16.6',
+        'arxiv-auth==0.4.2rc1',
         'flask==1.0.2',
         'bleach==3.1.0',
         'unidecode==1.0.23',
@@ -24,7 +24,8 @@ setup(
         'mypy_extensions==0.4.1',
         'gitpython==2.1.11',
         'py-gfm==0.1.4',
-        'pyyaml==5.1b1'
+        'pyyaml==5.1b1',
+        'gitdb2==2.0.6'
     ],
     include_package_data=True
 )
